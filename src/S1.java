45ec037c1174d1f5c101628a2c74358d1b34da06
gowthamnagaraj94 <gowthamnagaraj1234@gmail.com>
2019-03-21 16:10:07 +0530

    sub

--- src/S1.java
+++ src/S1.java
@@ -3,8 +3,12 @@ public class S1 {
 private void add() {
         System.out.println("addition");
 }
+private void sub() {
+     System.out.println("sub");
+}
 public static void main(String[] args) {
 	S1 v = new S1();
 	v.add();
+	v.sub();
 }
 }
